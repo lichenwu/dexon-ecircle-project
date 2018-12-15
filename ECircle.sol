2c4d7a75db363f3fc0ee0b2fa14ae6e6b4d85728
Li Chen Wu <judy_19901103@hotmail.com>
2018-12-15 17:42:09 +0800

    complete postLend

--- ECircle.sol
+++ ECircle.sol
@@ -1,17 +1,20 @@
 pragma solidity >=0.4.22 <0.6.0;
 
-contract Rand{
+contract ECircle{
    uint8 private ORDER_TYPE_LOAN=1;
    uint8 private ORDER_TYPE_LEND=2;
    
-   order[ ] private orders;
+   uint256[] private ordersSeqArr;
+   mapping(uint256 => Order) private orders;
+   
    uint256 private current_interest;
    uint256 private orderNoSeq = 0;
 
-   struct order {
+   struct Order {
       uint256 orderNo;
       uint8 orderType;
       
+      address postUser;
       uint256 amount;
       //uint256 interest;
       uint256 round;
@@ -19,14 +22,35 @@ contract Rand{
    }
 
     
-    function postLoan(uint256 amount, uint256 borrowingTime, uint256 round) public returns (bool){
-        order memory one;
+    function postLoan(uint256 amount, uint256 borrowingTime, uint256 round) public returns (uint256){
+        return genOrder(amount, borrowingTime, round, ORDER_TYPE_LOAN);
+    }
+    
+    function postLend(uint256 amount, uint256 borrowingTime, uint256 round) public returns (uint256){
+        return genOrder(amount, borrowingTime, round, ORDER_TYPE_LEND);
+    }
+    
+    function genOrder(uint256 amount, uint256 round, uint256 borrowingTime, uint8 orderType) 
+        private returns (uint256){
+        
+        Order memory one;
         one.orderNo=genOrderNo();
-        one.orderType=ORDER_TYPE_LOAN;
+        one.orderType=orderType;
+        one.postUser = msg.sender;
         one.amount = amount;
         one.round = round;
         one.borrowingTime = borrowingTime;
-        orders.push(one);
+        
+        ordersSeqArr.push(one.orderNo);
+        orders[one.orderNo]=one;
+
+        return orders[one.orderNo].orderNo;
+    }
+    
+    function getOrder(uint256 orderNo) public view returns(uint256,uint8,uint256,uint256,uint256){
+        Order memory one = orders[orderNo];
+        
+        return (one.orderNo, one.orderType, one.amount, one.round, one.borrowingTime);
     }
     
     // function getCurrentInterest() view private returns (uint256){
@@ -34,7 +58,6 @@ contract Rand{
     // }
     
     function genOrderNo()  private returns(uint256){
-        return orderNoSeq++;
+        return ++orderNoSeq;
     }
-    //The public testnet endpoint is available at: https://api-testnet.dexscan.org/v1/network/rpc
 }
